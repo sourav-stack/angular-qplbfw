5bb2228509a9f936fa9f831e1cf7a0d7ffe8b49b
sourav-stack <sourav.jha2809@gmail.com>
2020-03-24 00:46:38 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  myForm: FormGroup;
+
+   constructor(public fb: FormBuilder) {
+
+    this.myForm = this.fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8)]]});
+
+  }
+
+  onSubmit(form){
+    console.log(form.value);
+  }
+}
